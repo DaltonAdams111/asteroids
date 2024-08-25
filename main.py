ffa41747b273475083ca6c5d88ba8cade5b81ca9
Dalton <DaltonAdams111@gmail.com>
2024-08-24 23:42:06 -0500

    added Asteroid class

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    clock = pygame.time.Clock()
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
 
-    updatable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable, drawable)
+	Player.containers = (updatable, drawable)
+	Asteroid.containers = (updatable, drawable, asteroids)
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    dt = 0
+	dt = 0
 
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
 
-        for obj in updatable:
-            obj.update(dt)
+		for obj in updatable:
+			obj.update(dt)
 
-        screen.fill("black")
+		screen.fill("black")
 
-        for obj in drawable:
-            obj.draw(screen)
+		for obj in drawable:
+			obj.draw(screen)
 
-        pygame.display.flip()
+		pygame.display.flip()
 
-        # limit the framerate to 60 FPS
-        dt = clock.tick(60) / 1000
-
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
