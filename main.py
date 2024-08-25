8bb791cdb593fb52abb23687438fd3ba1db33a33
Dalton <DaltonAdams111@gmail.com>
2024-08-24 23:21:27 -0500

    added containers for updating and drawing

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
 import pygame
 from constants import *
-from player import *
+from player import Player
+
 
 def main():
-	pygame.init()
-	clock = pygame.time.Clock()
-	dt = 0
-	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-	pygame.display.set_caption("Asteroids")
-	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
-	while True:
-		for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				return
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
 
-		player.update(dt)
-		screen.fill("black")
-		player.draw(screen)
-		pygame.display.flip()
-		dt = clock.tick(60) / 1000
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
-	main()
+    main()
