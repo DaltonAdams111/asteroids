7431193b530146637210a92c75d6b94612c52bdd
Dalton <DaltonAdams111@gmail.com>
2024-09-04 22:07:14 -0500

    added container for shots

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def main():
 	Player.containers = (updatable, drawable)
 	Asteroid.containers = (updatable, drawable, asteroids)
 	AsteroidField.containers = (updatable)
-	Shot.containers = (updatable, drawable)
+	Shot.containers = (shots, updatable, drawable)
 
 	player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	asteroidfield = AsteroidField()
